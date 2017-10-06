958e435936808006d0a431f3ef1f386e4b7bd45c
Rangan <rm48@rice.edu>
2017-10-06 04:12:23 -0500

    implemented Dijkstra's algorithm - comments & docstrings missing

--- Shortest Path Models/Dijkstra.py
+++ Shortest Path Models/Dijkstra.py
@@ -10,18 +10,40 @@ def dijkstra(graph, source_node):
     """
     dist = dict()
     prev_node = dict()
+    Q = deque()
 
     for node in graph.keys():
         dist[node] = float("inf")
         prev_node[node] = None
+        Q.append(node)
 
     dist[source_node] = 0
 
-    Q = deque(sorted(dist[source_node].values()))
-
     while len(Q):
-        next_node = Q.popleft()
-        
+        current_node = determine_min_dist(Q, dist)
+        Q.remove(current_node)
+        for neighbor in graph[current_node].keys():
+            current_dist = dist[current_node] + graph[current_node][neighbor]
+            if current_dist < dist[neighbor]:
+                dist[neighbor] = current_dist
+                prev_node[neighbor] = current_node
 
+    return dist, prev_node
+
+
+def determine_min_dist(Q, dist):
+    """
+
+    :param Q:
+    :param dist:
+    :return:
+    """
+    if len(Q):
+        current_node = Q.index(0)
+        current_dist = float("inf")
+        for node in Q:
+            if dist[node] < current_dist:
+                current_node = node
+        return current_node
 
 
