5eeb69b3f528fcd3119f734d4cb818196bc6084e
Rangan <rm48@rice.edu>
2017-10-06 04:56:31 -0500

    comments & docstrings for dijkstra's algorithm

--- Shortest Path Models/Dijkstra.py
+++ Shortest Path Models/Dijkstra.py
@@ -3,47 +3,48 @@ from collections import deque
 
 def dijkstra(graph, source_node):
     """
-
-    :param graph:
-    :param source_node:
-    :return:
+    Given a graph represented in the adjacency list form and a source node, returns
+    the shortest path from the source node to every other node
     """
-    dist = dict()
-    prev_node = dict()
-    Q = deque()
+    dist = dict()  # initialize a mapping of nodes to distance to source node
+    prev_node = dict()  # initialize a mapping of nodes to their respective previous nodes in the shortest path
+    Q = deque()  # initialize an empty queue
 
+    # for every node in the graph
     for node in graph.keys():
-        dist[node] = float("inf")
-        prev_node[node] = None
-        Q.append(node)
+        dist[node] = float("inf")  # set the distance to the source node as negative infinity
+        prev_node[node] = None  # set the previous node to null
+        Q.append(node)  # add the node to the queue
 
-    dist[source_node] = 0
+    dist[source_node] = 0  # set the distance of the source node to itself to 0
 
+    # while Q is non-empty
     while len(Q):
-        current_node = determine_min_dist(Q, dist)
-        Q.remove(current_node)
-        for neighbor in graph[current_node].keys():
-            current_dist = dist[current_node] + graph[current_node][neighbor]
-            if current_dist < dist[neighbor]:
-                dist[neighbor] = current_dist
-                prev_node[neighbor] = current_node
+        current_node = determine_min_dist(Q, dist)  # determine the nearest node to the source node
+        Q.remove(current_node)  # remove the nearest node
+        for neighbor in graph[current_node].keys():  # for each neighbor of the nearest node
+            # compute the distance from the neighboring node to the source node
+            neighbor_dist = dist[current_node] + graph[current_node][neighbor]
+            # if the current neighbor distance is lower than the recorded neighbor distance
+            if neighbor_dist < dist[neighbor]:
+                dist[neighbor] = neighbor_dist  # replace the old neighbor distance with the new one
+                prev_node[neighbor] = current_node  # update the new previous node as the current node
 
-    return dist, prev_node
+    return dist, prev_node  # return the distance and previous node mappings
 
 
 def determine_min_dist(Q, dist):
     """
-
-    :param Q:
-    :param dist:
-    :return:
+    Given a queue of nodes and a mapping of nodes to their shortest distance to the
+    source node, returns the node nearest to the source node, assuming the input queue
+    is non-empty
     """
-    if len(Q):
-        current_node = Q.index(0)
-        current_dist = float("inf")
-        for node in Q:
-            if dist[node] < current_dist:
-                current_node = node
-        return current_node
+    if len(Q):  # if the input queue is non-empty
+        current_node = Q.index(0)  # initialize the current node to be the first node in the queue
+        current_dist = float("inf")  # initialize the current distance to negative infinity
+        for node in Q:  # for every node in the queue
+            if dist[node] < current_dist:  # if the source node is nearer to the iterated node than the current node
+                current_node = node  # replace the current node with the iterated node
+        return current_node  # return the final current node
 
 
