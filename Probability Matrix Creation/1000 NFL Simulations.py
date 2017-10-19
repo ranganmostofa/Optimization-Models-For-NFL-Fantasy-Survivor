87ffd9c3aa5d30b2413b728d01f62f88d76324ca
Marcob1996 <mib2@rice.edu>
2017-10-18 19:29:26 -0500

    NFL Season Simulation

--- Probability Matrix Creation/1000 NFL Simulations.py
+++ Probability Matrix Creation/1000 NFL Simulations.py
@@ -45,8 +45,10 @@ for i in range(100):
     Results = nfl_simulation(week, Elo_Rankings, Home_Away, Schedule, [], [], Win_Total, 0, 1)
     check = Results[2]
     weeks_correct = len(check) - 1
-    if sum(check[16]) > 0:
-        weeks_correct = 17
+    if len(check) == 17:
+        if sum(check[16]) > 0:
+            weeks_correct = 17
     Weeks_Successful.append(weeks_correct)
     Runs.append(Weeks_Successful)
+    print(i)
 write_csv('NFL Simulations2.csv', Runs)
