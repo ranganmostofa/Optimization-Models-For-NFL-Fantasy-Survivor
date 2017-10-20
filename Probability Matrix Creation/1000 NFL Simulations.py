1a0cc143513864532620f5716480825065f720d4
ranganmostofa11 <rm48@rice.edu>
2017-10-20 14:14:57 -0500

    NFL Simulation 4

--- Probability Matrix Creation/1000 NFL Simulations.py
+++ Probability Matrix Creation/1000 NFL Simulations.py
@@ -41,7 +41,7 @@ week = 1
 Weeks_Successful = []
 Probability = []
 Runs = []
-for i in range(333):
+for i in range(500):
     Results = nfl_simulation(week, Elo_Rankings, Home_Away, Schedule, [], [], Win_Total, 0, 1)
     weeks_survived = Results[0]
     probability_at_current_week = Results[1]
@@ -50,4 +50,4 @@ for i in range(333):
     print(i)
 Runs.append(Weeks_Successful)
 Runs.append(Probability)
-write_csv('NFL Simulations 3.csv', Runs)
+write_csv('NFL Simulations 7.csv', Runs)
